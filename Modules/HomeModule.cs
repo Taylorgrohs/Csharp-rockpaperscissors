cf6912efd0a65b1798931ee6be8a8e833d4af2df
taylor <taylorgrohs@gmail.com>
2016-02-17 10:40:36 -0800

    add homemodule

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,25 @@
+using Nancy;
+using System;
+using System.Collections.Generic;
+using RockPaperCookout;
+
+namespace TheRocksCookout
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Post["/winner"] = _ => {
+        var player1 = Request.Form["player1"];
+        var player2 = Request.Form["player2"];
+        RockPaperScissors newGame = new RockPaperScissors();
+        string winner = newGame.DoBattle(player1, player2);
+
+        return View["results.cshtml", winner];
+      };
+    }
+  }
+}
--- Objects/RockPaper.cs
+++ Objects/RockPaper.cs
@@ -5,7 +5,7 @@ namespace RockPaperCookout
 {
   public class RockPaperScissors
   {
-    public bool DoBattle(string player1, string player2)
+    public string DoBattle(string player1, string player2)
     {
       if (player1 == player2)
       {
@@ -13,27 +13,27 @@ namespace RockPaperCookout
       }
       else if (player1 == "rock" && player2 == "scissors")
       {
-        return "Player 1 Wins!"
+        return "Player 1 Wins!";
       }
       else if (player1 == "scissors" && player2 == "rock")
       {
-        return "Player 2 Wins!"
+        return "Player 2 Wins!";
       }
       else if (player1 == "paper" && player2 == "rock")
       {
-        return "Player 1 Wins!"
+        return "Player 1 Wins!";
       }
       else if (player1 == "rock" && player2 == "paper")
       {
-        return "Player 2 Wins!"
+        return "Player 2 Wins!";
       }
       else if (player1 == "scissors" && player2 == "paper")
       {
-        return "Player 1 Wins!"
+        return "Player 1 Wins!";
       }
       else if (player1 == "paper" && player2 == "scissors")
       {
-        return "Player 2 Wins!"
+        return "Player 2 Wins!";
       }
       else
       {
